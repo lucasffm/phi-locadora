e50cb2013cabf6df1f7162f6198aa0a675c4110b
Lucas Freitas <lucasffm@gmail.com>
2021-08-29 04:31:08 -0300

    1.2

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,7 @@ export class AppService {
     return {
       message: 'Phi Locadora',
       docs: `http://${process.env.VIRTUAL_HOST}/docs`,
-      version: 1.1
+      version: 1.2,
     };
   }
 }
